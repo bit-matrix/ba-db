8e6143196c9d5e7a631d21add6bf3d8b84ca7bce
Penguen Umut <uuisik@gmail.com>
2021-12-14 19:11:45 +0300

    models

---
+++ src/models/PoolTx.ts
@@ -0,0 +1,12 @@
+import { CommitmentTx } from "./CommitmentTx";
+
+export type PoolTx = {
+  block_hash: string;
+  block_height: number;
+  txs: [
+    {
+      txid: string;
+      commitmentTx: CommitmentTx;
+    }
+  ];
+};

24edbcbd2121e8ee65f1b082d4c12193a7bddb7b
Penguen Umut <uuisik@gmail.com>
2021-12-15 23:30:02 +0300

    local env

--- src/env.ts
+++ src/env.ts
@@ -1,2 +1,4 @@
+// dev local => yarn dev
+// export const DATA_DIR: string = process.env.DATA_DIR || "dist/data-dir/";
 export const DATA_DIR: string = process.env.DATA_DIR || "/ba-db/data-dir/";
 export const LISTEN_PORT: number = Number(process.env.PORT || 8899);

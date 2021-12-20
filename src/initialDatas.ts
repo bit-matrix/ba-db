84b5617e44a869b11cbce73a0ca2dcc7af34c93c
Penguen Umut <uuisik@gmail.com>
2021-12-20 20:18:53 +0300

    type fix

--- src/initialDatas.ts
+++ src/initialDatas.ts
@@ -36,7 +36,7 @@ export const POOLS: Pool[] = [
     },
     synced: false,
     syncedBlock: { block_hash: "721d3a1c587ad367bc8982ba9cb0e36c4136efdd1f240f286c9bc19504f3cb69", block_height: 131275 },
-    recentBlock: { block_hash: "721d3a1c587ad367bc8982ba9cb0e36c4136efdd1f240f286c9bc19504f3cb69", block_height: 131275 },
+    recentBlockHeight: 131275,
     active: true,
   },
 ];

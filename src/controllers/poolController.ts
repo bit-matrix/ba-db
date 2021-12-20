8611558a2a0890a85508bd22747f150e0b5be4f6
Penguen Umut <uuisik@gmail.com>
2021-12-20 20:12:13 +0300

    pool post controller, route

--- src/controllers/poolController.ts
+++ src/controllers/poolController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
+import { Pool } from "../models/Pool";
 import { PoolProvider } from "../providers/PoolProvider";
+import { isPoolAsset } from "./common";
 
 export const poolController = {
   getAll: async (req: Request, res: Response, next: NextFunction) => {
@@ -17,6 +19,23 @@ export const poolController = {
       const provider = await PoolProvider.getProvider();
       const result = await provider.get(req.params.asset);
       return res.status(200).send(result);
+    } catch (error) {
+      return res.status(501).send({ status: false, error });
+    }
+  },
+
+  post: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (req.body.id) {
+        await isPoolAsset(req.body.id);
+
+        const updatedPool = <Pool>req.body;
+        const provider = await PoolProvider.getProvider();
+        await provider.put(updatedPool.id, updatedPool);
+        return res.status(200).send({ status: true });
+      } else {
+        return res.status(501).send({ status: false, error: "Pool not found" });
+      }
     } catch (error) {
       return res.status(501).send({ status: false, error });
     }

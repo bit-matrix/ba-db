7d68a4f9038f7c59c254065210836fde8566568c
Penguen Umut <uuisik@gmail.com>
2021-12-25 17:33:41 +0300

    prod env fixed

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,2 @@
-// export const DATA_DIR: string = process.env.DATA_DIR || "/datavolume1/";
-export const DATA_DIR: string = process.env.DATA_DIR || "/dist/data-dir/";
+export const DATA_DIR: string = process.env.DATA_DIR || "/datavolume1/";
 export const LISTEN_PORT: number = Number(process.env.PORT || 8899);

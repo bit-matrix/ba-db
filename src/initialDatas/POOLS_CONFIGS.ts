ee1f743121acabefcbcb06b794fa6a91b8053ceb
Penguen Umut <uuisik@gmail.com>
2022-02-02 19:45:07 +0300

    pool8

--- src/initialDatas/POOLS_CONFIGS.ts
+++ src/initialDatas/POOLS_CONFIGS.ts
@@ -1,4 +1,5 @@
 import { BmConfig, Pool } from "@bitmatrix/models";
 import { pool3 } from "./pool3";
+import { pool8 } from "./pool8";
 
-export const POOLS_CONFIGS: { pool: Pool; config: BmConfig }[] = [pool3];
+export const POOLS_CONFIGS: { pool: Pool; config: BmConfig }[] = [pool8, pool3];

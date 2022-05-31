2ebdb3088fd41af4c438a400107b3afc56580ecd
huseyinsartas <hsynsrtss@gmail.com>
2022-05-31 17:45:35 +0300

    poolController updated.

--- src/controllers/poolController.ts
+++ src/controllers/poolController.ts
@@ -1,13 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import { Pool } from "@bitmatrix/models";
-import { PoolProvider } from "../providers/PoolProvider";
-import { isPoolAsset } from "./common";
+import { poolBusiness } from "../business/poolBusiness";
 
 export const poolController = {
   getAll: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const provider = await PoolProvider.getProvider();
-      const result = await provider.getMany();
+      const result = await poolBusiness.getPools();
       return res.status(200).send(result);
     } catch (error) {
       return res.status(501).send({ status: false, error });
@@ -16,8 +13,7 @@ export const poolController = {
 
   get: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const provider = await PoolProvider.getProvider();
-      const result = await provider.get(req.params.asset);
+      const result = await poolBusiness.getPool(req.params.asset);
       return res.status(200).send(result);
     } catch (error) {
       return res.status(501).send({ status: false, error });
@@ -27,13 +23,8 @@ export const poolController = {
   post: async (req: Request, res: Response, next: NextFunction) => {
     try {
       if (req.body.id) {
-        // commented for new pool insert
-        // await isPoolAsset(req.body.id);
-
-        const updatedPool = <Pool>req.body;
-        const provider = await PoolProvider.getProvider();
-        await provider.put(updatedPool.id, updatedPool);
-        return res.status(200).send({ status: true });
+        const result = await poolBusiness.postPool(req.body);
+        return res.status(200).send({ status: result });
       } else {
         return res.status(501).send({ status: false, error: "Pool id not found" });
       }

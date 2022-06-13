e4143b6328bbc725dbf186a92e6eccf41e2fcfd6
huseyinsartas <hsynsrtss@gmail.com>
2022-06-13 17:44:35 +0300

    poolService

--- src/services/poolService.ts
+++ src/services/poolService.ts
@@ -1,33 +1,12 @@
-import { Pool } from "@bitmatrix/models";
 import { PoolProvider } from "../providers/PoolProvider";
-import { calcTokenPrice, deepCopy } from "../utils/helper";
+import { calcTokenPrice } from "../utils/helper";
 
 export const poolService = {
   getPools: async () => {
     const provider = await PoolProvider.getProvider();
     const pools = await provider.getMany();
     const newPools = calcTokenPrice(pools);
-    if (newPools.length > 0) {
-      let sortedPools: Pool[] = [];
-      let i: number = 0;
-      while (i < newPools.length) {
-        i++;
-        if (newPools[i + 1] && newPools[i].quote.ticker === newPools[i + 1].quote.ticker && newPools[i].token.ticker === newPools[i + 1].token.ticker) {
-          sortedPools = pools.sort((a, b) => {
-            if (b.tokenPrice! < a.tokenPrice!) {
-              return -1;
-            } else if (b.tokenPrice! > a.tokenPrice!) {
-              return 1;
-            } else {
-              return 0;
-            }
-          });
-        } else {
-          sortedPools = deepCopy(newPools);
-        }
-      }
-      return sortedPools;
-    }
+    return newPools;
   },
 
   getPool: async (asset: string) => {

8f4b6dd137522b5250dd302de1e6e2b1a5410d5b
afarukcali <afarukcali@gmail.com>
2022-06-14 17:48:10 +0300

    usdt and lbtc asset hash config.

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,4 +1,5 @@
 import { Pool } from "@bitmatrix/models";
+import { LBTC_ASSET_HASH, USDT_ASSET_HASH } from "../config";
 
 export const deepCopy = <T>(original: T): T => {
   return JSON.parse(JSON.stringify(original));
@@ -7,13 +8,14 @@ export const deepCopy = <T>(original: T): T => {
 export const calcTokenPrice = (pools: Pool[]) => {
   const clonedPools = deepCopy(pools);
   clonedPools.forEach((pool) => {
-    if (pool.quote.ticker === "tL-BTC") {
+    if (pool.quote.assetHash === LBTC_ASSET_HASH) {
       pool.tokenPrice = Number(pool.token.value) / Number(pool.quote.value);
-    } else if (pool.quote.ticker === "tL-USDt") {
+    } else if (pool.quote.assetHash === USDT_ASSET_HASH) {
       pool.tokenPrice = Number(pool.quote.value) / Number(pool.token.value);
     } else {
       pool.tokenPrice = 0;
     }
   });
+
   return clonedPools;
 };

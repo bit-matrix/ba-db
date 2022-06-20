6bb9712886a0de55ef90b3b89da9fa014458bfbd
afarukcali <afarukcali@gmail.com>
2022-06-20 18:07:39 +0300

    Pool data with socket test.

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,10 @@ import ptxRoutes from "./routes/ptxRoutes";
 import ptxCtxRoutes from "./routes/ptxCtxRoutes";
 import configRoutes from "./routes/configRoutes";
 import appSyncRoutes from "./routes/appSyncRoutes";
+import { Server } from "socket.io";
 import { appChecker } from "./appChecker";
+import { PoolProvider } from "./providers/PoolProvider";
+import { poolService } from "./services/poolService";
 
 const onExit = async () => {
   console.log("BA DB Service stopped.");
@@ -26,6 +29,20 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
 app.get("/", async (req, res, next) => {
   res.send("hello from ba-db");
 });
@@ -37,6 +54,13 @@ app.use("/ptx-ctx", ptxCtxRoutes);
 app.use("/config", configRoutes);
 app.use("/appSync", appSyncRoutes);
 // app.use("/clear", clearRoutes);
+
+// test uri
+setInterval(async () => {
+  const pools = await poolService.getPools();
+
+  io.emit("pools", pools);
+}, 5000);
 
 appChecker().then(() => {
   server.listen(LISTEN_PORT, () => {

ea67512489e05195ea5d2497a14e30d787c7dd3b
reyhanboydag <reyhanboydag@gmail.com>
2022-09-02 00:48:28 +0300

    env.ts updated

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,15 @@
-export const DATA_DIR: string = process.env.DATA_DIR || "/datavolumemulti/";
-// export const DATA_DIR: string = process.env.DATA_DIR || "/dist/data-dir/";
-export const LISTEN_PORT: number = Number(process.env.PORT || 4499);
+import { AppSync } from "@bitmatrix/models";
+
+const db_port = process.env.DB_PORT || "8001";
+
+export const DATA_DIR: string = (process.env.API_INTERNAL_DATA_DIR || "/datavolumemulti") + "/";
+export const LISTEN_PORT: number = Number(db_port);
+
+export const APP_NAME: string = "testnetbitmatrix";
+
+export const INITIAL_BLOCK: AppSync = {
+  bestBlockHeight: 493457,
+  blockHash: "59d00accd70275b67ba152480de43de47f988ed6cc260d37212410c80f95087d",
+  blockHeight: 493457,
+  synced: false,
+};

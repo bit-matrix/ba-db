9097a9722715e802f7a0a7e8580807be1e4e7df2
afarukcali <afarukcali@gmail.com>
2022-09-11 18:45:20 +0300

    initial block updated.

--- src/env.ts
+++ src/env.ts
@@ -6,11 +6,11 @@ const db_internal_data_dir = process.env.DB_INTERNAL_DATA_DIR || "data_dir";
 export const DATA_DIR: string = db_internal_data_dir + "/";
 export const LISTEN_PORT: number = Number(db_internal_port);
 
-export const APP_NAME: string = "testnetbitmatrix";
+export const APP_NAME: string = "mainnetbitmatrix";
 
 export const INITIAL_BLOCK: AppSync = {
-  bestBlockHeight: 510233,
-  blockHash: "213c6e75d27a70a186130908a38de0368032fc21c7374ef4a2a358b74b69e966",
-  blockHeight: 510233,
+  bestBlockHeight: 2003832,
+  blockHash: "148b8f9d71f8c51f43d1ef35630563bba3b654e95225cce8f5798722ecc30fac",
+  blockHeight: 2003832,
   synced: true,
 };

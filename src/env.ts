6285dae40b0184688c925f8669f652f1235dae38
afarukcali <afarukcali@gmail.com>
2022-09-11 15:09:21 +0300

    initial block updated.

--- src/env.ts
+++ src/env.ts
@@ -9,8 +9,8 @@ export const LISTEN_PORT: number = Number(db_internal_port);
 export const APP_NAME: string = "testnetbitmatrix";
 
 export const INITIAL_BLOCK: AppSync = {
-  bestBlockHeight: 507193,
-  blockHash: "dabebfbd109cd85e2271aa73ad6e4d3edf877b1b64982993da093e9df80c9ce4",
-  blockHeight: 507193,
-  synced: false,
+  bestBlockHeight: 510233,
+  blockHash: "213c6e75d27a70a186130908a38de0368032fc21c7374ef4a2a358b74b69e966",
+  blockHeight: 510233,
+  synced: true,
 };
